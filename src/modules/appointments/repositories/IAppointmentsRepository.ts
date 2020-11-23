5adfb58291f01e09a1e6401933bece83a5157afc
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-11-23 09:04:07 -0300

    estrutura de pastas do projeto sendo remodelada ao padrão DDD

---
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,9 @@
+import Appointment from "../infra/typeorm/entities/Appointment";
+
+import Appointments from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+export default interface IAppointmentsRepository{
+  create(data: ICreateAppointmentDTO): Promise<Appointments>;
+  findByDate(date: Date): Promise<Appointments | undefined>;
+}
